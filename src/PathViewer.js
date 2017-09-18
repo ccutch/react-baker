92130d4c3152da2897a9a658b2d909e7a3e49a78
Connor <cmccutch@starbucks.com>
2017-09-18 14:52:48 -0700

    updating

--- src/PathViewer.js
+++ src/PathViewer.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { observer } from 'mobx-react'
 import { breadcrumbs } from './store'
-import PropTypes from 'prop-types'
 
 export const PathViewer = observer(({ containerClass, render = b => null }) => (
   <div className={containerClass}>{breadcrumbs.map(render)}</div>
